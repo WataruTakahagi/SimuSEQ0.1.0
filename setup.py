3143aeb20fd14539f6475d786970a8f7453fdcec
Kozo Nishida <knishida@riken.jp>
2015-09-24 17:21:18 +0900

    add setup()

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='SimuSEQ',
+    version='0.1.0',
+    author='Wataru Takahagi',
+    author_email='t12525wt@sfc.keio.ac.jp',
+    url='https://github.com/WataruTakahagi/SimuSEQ',
+    py_modules=['simuseq'],
+    description='Microarray analysis tool for whole-ecoli project',
+    keywords='simulation visualization bioinformatics',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
